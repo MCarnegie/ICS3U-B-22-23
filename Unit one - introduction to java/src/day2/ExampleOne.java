48e736cdcb2d1e55c3c63762c3513d8e1f7c4865
MCarnegie <mcarnegie@bayviewglen.ca>
2022-09-09 11:07:23 -0400

    Initial Commit

---
+++ Unit one - introduction to java/src/day2/ExampleOne.java
@@ -0,0 +1,21 @@
+package day2;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println();
+
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.println("D");
+        System.out.print("E");
+        System.out.println("F");
+    }
+}
---
+++ Unit one - introduction to java/src/day2/ExampleTwo.java
@@ -0,0 +1,12 @@
+package day2;
+
+/* This example is about escape sequences */
+public class ExampleTwo {
+  public static void main(String[] args) {
+    System.out.println("Computer Scie\nce is the \"Best\" class ever!");
+
+    // escape sequence is meant to give antoehr meaning to a charecter
+    // Ap cares about \" \n \\
+    // \n is new line
+  }  
+}
