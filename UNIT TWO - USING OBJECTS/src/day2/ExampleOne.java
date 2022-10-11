abb5674375980c001b284866c3bdb9c97404b516
MCarnegie <mcarnegie@bayviewglen.ca>
2022-10-11 09:33:46 -0400

    Strings, Substrings, and classes

---
+++ UNIT TWO - USING OBJECTS/src/day2/ExampleOne.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println(myMethod(4, false));
+     }
+  
+     public static double myMethod(int a, boolean b) {
+        a++;
+        b = true;
+  
+        return 7.6;
+     }
+}
