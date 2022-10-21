4dedcbe3bcaffc164e74a8727bc8419dd03926a1
MCarnegie <mcarnegie@bayviewglen.ca>
2022-10-21 15:47:30 -0400

    added my coding bat homework

---
+++ Unit Three - Boolean Expressions and if Statements/src/day1/CodingBatLogic1.java
@@ -0,0 +1,113 @@
+package day1;
+
+public class CodingBatLogic1 {
+    
+    public boolean love6(int a, int b) {
+            if(a == 6 || b == 6)
+              return true;
+            else if (a + b == 6 || Math.abs(a-b) == 6)
+              return true;
+            else
+              return false;
+            
+    }
+
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(isWeekend == false && cigars>=40 && cigars<=60)
+            return true;
+        if(isWeekend == true && cigars>=40)
+            return true;
+          
+        
+        return false;
+    }
+
+    public int dateFashion(int you, int date) {
+        if(you<=2 || date<=2)
+          return 0;
+        
+        else if(you>=8  || date>=8)
+          return 2;
+          
+        return 1;
+        
+        
+    }
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if(isSummer == true && temp>=60 && temp<=100)
+          return true;
+        
+        if(temp>=60 && temp<=90)
+          return true;
+        
+        return false;
+    }
+    
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if(isBirthday)
+          speed -= 5;
+        
+        if(speed<=60)
+          return 0;
+        if(speed>=61 && speed<=80)
+          return 1;
+        if(speed >= 81)
+          return 2;
+          
+        return 5;
+        
+    }
+
+    public int sortaSum(int a, int b) {
+      if((a+b)>=10 && (a+b)<=19 )
+      return 20;
+      
+      return a+b;
+    }
+
+    public String alarmClock(int day, boolean vacation) {
+  
+      if(vacation && day>=1 && day<=5)
+      return "10:00";
+      else if(vacation && day == 6)
+      return "off";
+      else if(vacation && day == 0)
+      return "off";
+      else if(day>=1 && day<=5)
+      return "7:00";
+      else
+      return "10:00";
+      
+    }
+  
+    public boolean in1To10(int n, boolean outsideMode) {
+      if(outsideMode && n>=10)
+      return true;
+      if(outsideMode && n<=1)
+      return true;
+      if(!outsideMode&&n<=10 && n>=1)
+      return true;
+      
+      return false;
+    }
+  
+    public boolean specialEleven(int n) {
+      if((n%11) == 0 || ((n-1)%11) == 0 )
+      return true;
+      
+      return false;
+    }
+    
+    public boolean more20(int n) {
+      if((n-2)%20 == 0 || ((n-1)%20) == 0 )
+      return true;
+      
+      return false;
+    }
+    
+    
+      
+      
+    
+}
